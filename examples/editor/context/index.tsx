cb70a1e747630f65b442e6573e0635e6e152782a
ZhangXuanZhi <zhangxuanzhi@outlook.com>
2020-10-30 14:33:34 +0800

    添加示例

--- examples/editor/context/index.tsx
+++ examples/editor/context/index.tsx
@@ -1,54 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GGEditor, { Flow, ContextMenu } from 'gg-editor';
 import WrappedClassComponent from './WrappedClassComponent';
 import WrappedFunctionComponent from './WrappedFunctionComponent';
 import styles from './index.less';
 import { Button, Menu } from 'antd';
-const data = {
-  nodes: [
-    {
-      id: '0',
-      label: 'Node',
-      x: 50,
-      y: 50,
-    },
-    {
-      id: '1',
-      label: 'Node',
-      x: 50,
-      y: 200,
-    },
-  ],
-  edges: [
-    {
-      label: 'Label',
-      source: '0',
-      sourceAnchor: 1,
-      target: '1',
-      targetAnchor: 0,
-    },
-  ],
-};
-
-const flowRef = React.createRef<Flow>();
-
-function addStep() {
-  const model = {
-    label: '新步骤',
-    x: 200,
-    y: 150,
-  };
-  flowRef.current?.graph?.addItem('node', model);
-}
-
-function deleteStep(item) {
-  flowRef.current?.graph?.removeItem(item);
-}
-function getData() {
-  console.log(flowRef.current?.graph?.save());
-}
 
 function App() {
+  const flowRef = React.createRef<Flow>();
+  const [flowData, setflowData] = useState({
+    nodes: [],
+    edges: [],
+  });
+  function addStep() {
+    const model = {
+      label: '新步骤',
+      x: 200,
+      y: 150,
+    };
+    flowRef.current?.graph?.addItem('node', model);
+  }
+
+  function deleteStep(item) {
+    flowRef.current?.graph?.removeItem(item);
+  }
+
+  function getData() {
+    console.log(flowRef.current?.graph?.save());
+  }
+
+  const data = {
+    nodes: [
+      {
+        id: '0',
+        label: 'Node',
+        x: 50,
+        y: 50,
+      },
+      {
+        id: '1',
+        label: 'Node',
+        x: 50,
+        y: 200,
+      },
+    ],
+    edges: [
+      {
+        label: 'Label',
+        source: '0',
+        sourceAnchor: 1,
+        target: '1',
+        targetAnchor: 0,
+      },
+    ],
+  };
+
+  function getSyncData() {
+    setflowData(data);
+  }
+
   return (
     <div>
       <div>
@@ -58,9 +67,13 @@ function App() {
         <Button onClick={getData} type="primary">
           获取数据
         </Button>
+
+        <Button onClick={getSyncData} type="primary">
+          异步加载数据
+        </Button>
       </div>
       <GGEditor>
-        <Flow className={styles.graph} ref={flowRef} data={data} />
+        <Flow className={styles.graph} ref={flowRef} data={flowData} />
         <ContextMenu
           type="node"
           renderContent={(item, position, hide) => {
